dc28a3eadebf363e9bc90afe7fa8a8ca11ac2ee3
Alberto <Albertopga@gmail.com>
2020-10-26 12:32:40 +0100

    start frontend

--- Backend/index.js
+++ Backend/index.js
@@ -2,16 +2,17 @@
 
 const mongoose = require("mongoose");
 const app = require("./app");
-const routes = require("./routes/card");
 const port = 3901;
 const url = "mongodb://localhost:27017/kot";
 
 mongoose.set("useFindAndModify", false); // disable old mongoose methods
 mongoose.Promise = global.Promise; // enable mongoose promesis
-mongoose //create the connection with mongodb
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, )
+//create the connection with mongodb
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Conexión realizada con exito");
+    //create te server to listen http requests
     app.listen(port, () => {
       console.log(`servidor corriendo en http://localhost:${port}`);
     });
